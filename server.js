b762a2565c14b53549f8d3d2884f3d4a0a73fcae
Suraj Shetty <surajshetty3416@gmail.com>
2018-03-10 23:08:18 +0530

    added server code

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
-const app = require('express')();
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-app.post('/screenshare', (req, res) => {
-    console.log(req.body);
-    res.send('ok');
+var express = require('express');//Importing Express
+var app = express();//Getting App From Express
+const port = 8080;//Creating A Constant For Providing The Port
+var io = require('socket.io').listen(app.listen(port));
+//Routing Request : http://localhost:port/
+sdpData = {};
+app.get('/', function (request, response) {
+    //Telling Browser That The File Provided Is A HTML File
+    response.writeHead(200, { "Content-Type": "text/html" });
+    //Passing HTML To Browser
+    response.write("The Server Is <strong>Working</strong>!");
+    //Ending Response
+    response.end();
 })
-
-app.listen(3445, () => console.log('Listening on 3445'));
+io.sockets.on("connection", function (socket) {
+	console.log('connected!');
+	socket.on("payload_from_share", (data) => {
+		data = getJson(data);
+		sdpData[data.id] = {
+			sdp: data.sdp
+		};
+		socket.on('join_request:' + id, () => {
+			socket.emit('sdp_for:'+ id, {
+				sdp : sdpData[id].sdp
+			});
+		});
+		socket.on('join_sdp:'+id, (data) => {
+			socket.emit('sdp_for_join:'+id, data);
+		});
+	})
+})
+//For Tracking When User Disconnects:
+io.sockets.on("disconnect", function (socket) {
+	//var socket is the socket for the client who has disconnected.
+})
